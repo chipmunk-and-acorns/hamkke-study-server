a466177abcbccd8952d5cd630d5e25a087b71ff2
chipmunk-dev <devquokkajeong@gmail.com>
2023-08-20 01:19:58 +0900

    #1 - validation 수정

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -5,7 +5,7 @@ export const memberValid = {
   register: [
     check('username').isEmail().withMessage('유효한 이메일 주소를 입력해주세요.'),
     check('password')
-      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]$/)
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-=_+[\]{}|;:'",.<>/?]).{8,16}$/)
       .withMessage(
         '비밀번호는 최소 1개의 소문자, 대문자, 숫자, 특수문자(!@#$%^&*()_+)를 포함해야 합니다.',
       )
@@ -37,6 +37,8 @@ export const memberValid = {
 
         return response.status(400).json({ errors: result });
       }
+
+      next();
     },
   ],
   login: [
@@ -51,5 +53,28 @@ export const memberValid = {
         max: 16,
       })
       .withMessage('비밀번호는 최소 8글자부터 최대 16글자까지 가능합니다.'),
+    (request: Request, response: Response, next: NextFunction) => {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        const result = errors
+          .array()
+          .reduce((acc: { [key: string]: string }, cur: ValidationError) => {
+            const error = cur as FieldValidationError;
+            const { path, msg } = error;
+
+            if (path in acc) {
+              acc[path] = acc[path] += `\n${msg}`;
+            } else {
+              acc[path] = msg;
+            }
+
+            return acc;
+          }, {});
+
+        return response.status(400).json({ errors: result });
+      }
+
+      next();
+    },
   ],
 };
