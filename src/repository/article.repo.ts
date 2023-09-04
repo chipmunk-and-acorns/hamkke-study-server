729878720aa071ae71b6247ef71d0c3eeb31ea3a
chipmunk-dev <devquokkajeong@gmail.com>
2023-09-04 18:33:55 +0900

    #2 - repository 함수 뼈대 구현

---
+++ src/repository/article.repo.ts
@@ -0,0 +1,8 @@
+interface ArticlePost {}
+
+export const saveArticle = async (article: ArticlePost) => {};
+export const findArticles = async () => {};
+export const findArticleById = async (articleId: number) => {};
+export const updateArticleById = async (articleId: number) => {};
+export const completeArticleById = async (articleId: number) => {};
+export const deleteArticleById = async (articleId: number) => {};
