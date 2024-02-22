d987652ebb55ec89dfc8649080504472cf2eb239
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-02-22 22:15:09 +0900

    모듈 설정
    캐쉬 모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,39 @@
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import {
-  ENV_DB_DATABASE_KEY,
-  ENV_DB_HOST_KEY,
-  ENV_DB_PASSWORD_KEY,
-  ENV_DB_PORT_KEY,
-  ENV_DB_USERNAME_KEY,
-} from 'common/const/env-keys.const';
+  ENV_POSTGRES_DATABASE_KEY,
+  ENV_POSTGRES_HOST_KEY,
+  ENV_POSTGRES_PASSWORD_KEY,
+  ENV_POSTGRES_PORT_KEY,
+  ENV_POSTGRES_USERNAME_KEY,
+} from 'src/const/env-keys.const';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `${process.env['NODE_ENV'] === 'production' ? '.env' : '.env.development.local'}`,
+      envFilePath: '.env',
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env[ENV_DB_HOST_KEY],
-      port: parseInt(process.env[ENV_DB_PORT_KEY]),
-      username: process.env[ENV_DB_USERNAME_KEY],
-      password: process.env[ENV_DB_PASSWORD_KEY],
-      database: process.env[ENV_DB_DATABASE_KEY],
+      host: process.env[ENV_POSTGRES_HOST_KEY],
+      port: parseInt(process.env[ENV_POSTGRES_PORT_KEY]),
+      username: process.env[ENV_POSTGRES_USERNAME_KEY],
+      password: process.env[ENV_POSTGRES_PASSWORD_KEY],
+      database: process.env[ENV_POSTGRES_DATABASE_KEY],
       entities: [],
       synchronize: true,
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      host: 'redis',
+      port: 6379,
     }),
   ],
   controllers: [AppController],
